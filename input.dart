8adfae4c26e85db45f0e0ff2a203c21dfee48627
hyojaeKwon <khj010909@gm.gist.ac.kr>
2022-02-19 13:41:48 +0900

    [add] the input of dart

---
+++ input.dart
@@ -0,0 +1,18 @@
+import 'dart:io';
+
+main(){
+  //Use stdout to print the info String
+  stdout.writeln('Please wirte your name');
+
+  //String input : readLineSync
+  
+  //optional -> null방지하기 위해
+  String? name = stdin.readLineSync();
+
+  //check the type
+  print(name.runtimeType);
+  
+  //print the variable with dollor($) sign
+  print('Your name is $name');  
+
+}
---
+++ playground.dart
@@ -0,0 +1,13 @@
+//dart:core
+import 'dart:core';
+
+//main안에 실행시킬 코드 넣는다.
+main(){
+  
+  //변수 선언
+  var fisrtString = "hello";
+  String secondString = "world";
+
+  print(fisrtString + ' ' +secondString);
+  // print('hello world');
+}
