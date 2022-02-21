f66682f2e9fc4d1b1d29b764f896ed0021cb6252
hyojaeKwon <khj010909@gm.gist.ac.kr>
2022-02-22 06:22:52 +0900

    api test

--- exampleApp/exapp/lib/main.dart
+++ exampleApp/exapp/lib/main.dart
@@ -4,19 +4,35 @@ import 'classMake.dart';
 import 'dart:async';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+// import './test.json';
 
-Future<User> fetchPost() async {
-  final response = await http.get(Uri.parse('https://jsonplaceholder.typicode.com/posts/1'));
 
+// Future<User> fetchPost() async {
+//   final response = await http.get(Uri.parse('https://jsonplaceholder.typicode.com/comments?postId=1'));
+//   // print(jsonDecode(response.body));
+//   if (response.statusCode == 200) {
+//     // 만약 서버로의 요청이 성공하면, JSON을 파싱합니다.
+//     return User.fromJson(jsonDecode(response.body));
+//   } else {
+//     // 만약 요청이 실패하면, 에러를 던집니다.
+//     throw Exception('Failed to load post');
+//   }
+// }
+
+
+//api get
+Future<String> getName(String name) async{
+  String newUrl = 'https://solution-challenge-hb6fjqbi3q-du.a.run.app/user/'+name;
+  final response = await http.get(Uri.parse(newUrl));
   if (response.statusCode == 200) {
     // 만약 서버로의 요청이 성공하면, JSON을 파싱합니다.
-    return User.fromJson(jsonDecode(response.body));
+    // print(response.body);
+    return response.body;
   } else {
     // 만약 요청이 실패하면, 에러를 던집니다.
     throw Exception('Failed to load post');
   }
 }
-//
 
 void main() {
   runApp(MyApp());
@@ -47,12 +63,9 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
     
   late Future<User> futureUser;
+  final myController = TextEditingController();
+  late String inputText;
 
-  @override
-  void initState(){
-    super.initState();
-    futureUser = fetchPost();
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -63,39 +76,56 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Fetch Data Example'),
+          title: const Text('Api Example'),
         ),
-        body: Center(
- 
-
-          child: FutureBuilder<User>(
-            future: futureUser,
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                return Column(
-                  
+        body:
+            Column(
+              children: [
+                Row(
+                  mainAxisSize: MainAxisSize.min,
                   children: [
-                    Text(snapshot.data!.title,
-                      style: TextStyle(
-                        color: Colors.red,
-                        fontWeight: FontWeight.w700,
-                        backgroundColor: Color.fromARGB(255, 12, 76, 129)
-                      )
+                  Container(
+                    width: 300,
+                    padding: EdgeInsets.all(10),
+                    child: TextField(
+                      maxLength: 5,
+                      controller: myController,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'enter your name',
+                        counterText: '',
+                      ),
+                    )
+                  ),
+                  OutlinedButton(
+                    onPressed: () => {
+                      setState((){
+                        String name = myController.text;
+                        getName(name).then((value){
+                          inputText = value;
+                        }).onError((error, stackTrace){
+                          inputText = 'error';
+                        });
+                      })
+                    },
+                    child: Text('Search'),
+                  )
+                  ],
+                ),
+                Container(
+                  child: Text(
+                    '$inputText',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold
                     ),
-                    Text(snapshot.data!.body),
-                    Text(snapshot.data!.user_id.toString())
-                  ],  
-                );
-
-              } else if (snapshot.hasError) {
-                return Text('${snapshot.error}');
-              }
-
-              // By default, show a loading spinner.
-              return const CircularProgressIndicator();
-            },
-          ),
-        ),
+                    ),
+                ) 
+              
+              ],
+            )
+            
+            
       ),
     );
   }
