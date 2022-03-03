0099183ec0a78c1e0a1f5dde691d015615691b51
hyojaeKwon <khj010909@gm.gist.ac.kr>
2022-03-04 03:52:58 +0900

    debuged

--- exampleApp/exapp/lib/main.dart
+++ exampleApp/exapp/lib/main.dart
@@ -1,7 +1,44 @@
 import 'package:flutter/material.dart';
 
 // main 함수 , the starting point of app
-void main() => runApp(MyApp());
+void main(){
+  runApp(Myapp());
+}
 //괄호 안에서는 위젯들어감
 
- 
+class Myapp extends StatelessWidget {
+  const Myapp({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'First app',
+      theme: ThemeData(
+        primarySwatch: Colors.blue
+      ),
+      home: MyHomePage(),
+    );    
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('first Text'),
+      ),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            Text('hello'),
+            Text('hello'),
+            Text('hello'),
+          ],
+        ),
+      ),
+    );
+  }
+}
