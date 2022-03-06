c94e552d6bda4a2728f4c960d3b73f2135ff7f28
hyojaeKwon <khj010909@gm.gist.ac.kr>
2022-03-06 19:30:59 +0900

    stateful widget

--- exampleApp/exapp/lib/main.dart
+++ exampleApp/exapp/lib/main.dart
@@ -1,90 +1,55 @@
 import 'package:flutter/material.dart';
 
-// main 함수 , the starting point of app
-void main(){
-  runApp(Myapp());
+void main() => runApp(Myapp);
+
+class MyApp extends StatefulWidget{
+
 }
-//괄호 안에서는 위젯들어감
 
-class Myapp extends StatelessWidget {
-  const Myapp({ Key? key }) : super(key: key);
+class MyAppState extends State<MyApp>{
+  late String name;
 
+  final myController = TextEditingController();
+  
+  
   @override
-  Widget build(BuildContext context) {
+  Widget bulid(BuildContext context){
+
     return MaterialApp(
-      title: 'First app',
       theme: ThemeData(
-        primarySwatch: Colors.blue
+        primarySwatch: Colors.red,
       ),
-      home: MyHomePage(),
-    );    
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({ Key? key }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('first Text'),
-        centerTitle: true,
-        backgroundColor: Colors.cyan,
-        elevation: 0.0,
-      ),
-
-      //body에 Padding 지정
-      body:  Center(
-        child: Column(
-            // crossAxisAlignment: CrossAxisAlignment.start,
-            // mainAxisAlignment: MainAxisAlignment.center,
+      home: Scaffold(
+        appBar: AppBar(),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Text('hello'),
-              Text('Second data',
-              style: TextStyle(
-                color: Colors.amber[800],
-                letterSpacing: 2.0,
-                fontSize: 28.0,
-                fontWeight: FontWeight.bold
+              Text('enter your name'),
+              TextField(
+                decoration: InputDecoration(
+                  alignLabelWithHint: true,
+                  border: OutlineInputBorder(),
+                  labelText: 'enter your name',
                 ),
+                controller: myController,
               ),
-              Text('hi from flutter'),
-              SizedBox(
-                height: 30.0,
+              FloatingActionButton(
+                onPressed: (() => {
+                  name = myController.text
+                } 
               ),
-              Text('Second data',
-              style: TextStyle(
-                color: Colors.amber[800],
-                letterSpacing: 2.0,
-                fontSize: 28.0,
-                fontWeight: FontWeight.bold
-                ),
-              ),
-              Divider(
-                height: 60.0,
-                color: Colors.grey[850],
-                thickness: 0.5,
-                endIndent: 30.0,
-              ),
-              Row(
-                children: <Widget>[
-                  Icon(Icons.check_circle_outline),
-                  SizedBox(
-                    width: 10.0,
-                  ),
-                  Text('using lightsaber',
-                  style: TextStyle(
-                    fontSize: 16.0,
-                    letterSpacing: 1.0,
-                  ),
-                  ),
-                ],
-              )
+              
             ],
           ),
+        ),
       ),
-        
-    );
+    )
   }
-} 
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
