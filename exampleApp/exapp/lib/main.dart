8f0dfd3fbf37982137c145a506c25a40085420b0
hyojaeKwon <khj010909@gm.gist.ac.kr>
2022-03-07 18:10:31 +0900

    google maps add

--- exampleApp/exapp/lib/main.dart
+++ exampleApp/exapp/lib/main.dart
@@ -1,67 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'dart:async';
 
-void main() => runApp(MyApp());
+void main() => runApp(MapSample());
  
 
-class MyApp extends StatefulWidget{
 
-
+class MapSample extends StatefulWidget {
   @override
-  MyAppState createState() => MyAppState();
-  
+  State<MapSample> createState() => MapSampleState();
 }
 
-class MyAppState extends State<MyApp>{
-  late String name = "";
+class MapSampleState extends State<MapSample> {
+  Completer<GoogleMapController> _controller = Completer();
 
-  final myController = TextEditingController();
-  
+  static final CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+
+  static final CameraPosition _kLake = CameraPosition(
+      bearing: 192.8334901395799,
+      target: LatLng(37.43296265331129, -122.08832357078792),
+      tilt: 59.440717697143555,
+      zoom: 19.151926040649414);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        primarySwatch: Colors.red,
+    return new Scaffold(
+      body: GoogleMap(
+        mapType: MapType.hybrid,
+        initialCameraPosition: _kGooglePlex,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
       ),
-      home: Scaffold(
-        appBar: AppBar(),
-
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text('enter your name'),
-
-              TextField(
-                decoration: InputDecoration(
-                  alignLabelWithHint: true,
-                  border: OutlineInputBorder(),
-                  labelText: 'enter your name',
-                ),
-                controller: myController,
-              ),
-
-              FloatingActionButton(
-                onPressed: () {
-                  setState((){
-                    name = myController.text;
-                  });
-                },
-              ),
-
-              Text(
-                'hello $name'
-              ),  
-            ],
-          ),
-        ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: _goToTheLake,
+        label: Text('To the lake!'),
+        icon: Icon(Icons.directions_boat),
       ),
     );
-    throw UnimplementedError();
-    
   }
 
-
-
-
- 
+  Future<void> _goToTheLake() async {
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
+  }
 }
--- exampleApp/exapp/test/future.dart
+++ exampleApp/exapp/test/future.dart
@@ -15,16 +15,30 @@ void startTask(){
   print(info1);
 }
 
-void accessData(){
+String accessData(){
+
+  String account;
   //give duration to code
   Duration time = Duration(
     seconds: 3
   );
-  sleep(time);
-  String info2 = 'access to the data';
-  print(info2);
+
+  if(time.inSeconds > 2){
+    Future.delayed(time, (){
+      account = '8500';
+      print(account);
+    });
+  }
+  else{
+    account = 'data';
+  }
+
+  return account;
+  
 }
 void fetchData(){
   String info3 = 'fecth the data to client';
   print(info3); 
 }
+
+//순서
